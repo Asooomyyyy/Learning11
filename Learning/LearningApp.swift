c64ce66cf3a9d03f5b49c3d07b2dd2b5bd61a8b0
asma suliman <asma14231@gmail.com>
2025-10-19 14:36:55 +0300

    Initial Commit

---
+++ Learning/LearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningApp.swift
+//  Learning
+//
+//  Created by Asma Abdullah Suliman on 26/04/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct LearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
